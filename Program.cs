370222ba9f84e9322f9f51cc4f955d9670b316f0
Danilo Costa <dandan_costa2222@outlook.com>
2025-06-22 05:52:03 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Genius
+{
+   static void Main()
+    {
+        Console.WriteLine("--- Genius ---\n");
+
+        Console.WriteLine("Dificuldades: \n");
+        Console.WriteLine("1 - Facil (8 botões)");
+        Console.WriteLine("2 - Medio (14 botões)");
+        Console.WriteLine("3 - Dificil (20 botões)");
+        Console.WriteLine("4 - Expert (31 botões)");
+
+        while (true)
+        {
+            Console.Write("Escolha a dificuldade (1 a 4): ");
+            if (int.TryParse(Console.ReadLine(), out nivel) && nivel >= 1 && nivel <= 4)
+                break;
+            Console.WriteLine("Opção inválida!");
+        }
+
+        int sequenciaMax = dificuldade[nivel];
+        List<char> sequencia = new List<char>();
+        char[] botoes = { 'R', 'G', 'Y', 'B' };
+
+        Console.WriteLine("Prepare-se para a sequência...");
+        Thread.Sleep(1000);
+
+        for (int rodada = 0; rodada < sequenciaMax; rodada++)
+        {
+            char proximo = botoes[random.Next(4)];
+            sequencia.Add(proximo);
+
+            Console.Clear();
+            Console.WriteLine($"Rodada {rodada + 1} de {sequenciaMax}");
+            Console.WriteLine("Memorize a sequência:");
+
+            MostrarSequencia(sequencia);
+
+            Console.Clear();
+            Console.WriteLine("Agora é sua vez! Digite a sequência usando:");
+            Console.WriteLine("R = Vermelho | G = Verde | Y = Amarelo | B = Azul");
+
+            for (int i = 0; i < sequencia.Count; i++)
+            {
+                Console.Write($"Botão #{i + 1}: ");
+                string entrada = Console.ReadLine()!.ToUpper();
+
+                if (entrada.Length != 1 || !"RGYB".Contains(entrada))
+                {
+                    Console.WriteLine("Entrada inválida. Você perdeu!");
+                    SomDerrota();
+                    return;
+                }
+
+                if (entrada[0] != sequencia[i])
+                {
+                    Console.WriteLine($"Errou! O correto era {colors[sequencia[i]]}. Você perdeu!");
+                    SomDerrota();
+                    return;
+                }
+            }
+
+            Console.WriteLine("Sequência correta! Próxima rodada...");
+            Thread.Sleep(1000);
+        }
+
+        // Venceu o jogo
+        Console.WriteLine("Parabéns! Você venceu o jogo!");
+
+        if (nivel == 4)
+            SomVitoriaEspecial();
+        else
+            SomVitoria();
+    }
+
+     static Random random = new Random();
+
+    static Dictionary<char, string> colors = new Dictionary<char, string>
+    {
+        {'R', "Vermelho" },
+        {'G', "Verde" },
+        {'Y', "Amarelo" },
+        {'B', "Azul" }
+    };
+
+    static Dictionary<int, int> dificuldade = new Dictionary<int, int>
+    {
+        {1, 8},
+        {2, 14},
+        {3, 20},
+        {4, 31}
+    };
+
+    static int nivel;
+
+
+    static void MostrarSequencia(List<char> sequencia)
+    {
+        foreach (char c in sequencia)
+        {
+            Console.WriteLine($"-> {colors[c]}");
+            Console.Beep(GetFrequencia(c), 500);
+            Thread.Sleep(200);
+        }
+    }
+
+    static int GetFrequencia(char cor)
+    {
+        return cor switch
+        {
+            'R' => 400,
+            'G' => 500,
+            'Y' => 600,
+            'B' => 700,
+            _ => 800
+        };
+    }
+
+    static void SomDerrota()
+    {
+        Console.Beep(300, 700);
+        Console.Beep(200, 700);
+        Console.Beep(100, 700);
+    }
+
+    static void SomVitoria()
+    {
+        Console.Beep(600, 300);
+        Console.Beep(800, 300);
+        Console.Beep(1000, 300);
+    }
+
+    static void SomVitoriaEspecial()
+    {
+        Console.Beep(900, 300);
+        Console.Beep(1100, 300);
+        Console.Beep(1300, 300);
+        Console.Beep(1500, 300);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # Genius
-Implemente o jogo #1 para um jogador em seus 4 níveis de dificuldade.
+
+## Sobre
+
+O brinquedo [Genius] (https://www.estrela.com.br/jogo-genius-estrela-100543353_est_pai/p) é um jogo eletrônico de memória popular nos anos 80. Lançado no Brasil pela [Estrela] (https://www.estrela.com.br/), é a versão brasileira do americano Simon, da [Hasbro] (https://products.hasbro.com/pt-br).
+
+[![Genius - Estrela](/img/genius.png)](https://estrela.vteximg.com.br/arquivos/ids/163355-1000-1000/Jogo-Genius-Produto-Estrela.jpg?v=636661399595430000)
+
+## Instruções
+
+O objetivo do jogo é repetir corretamente a sequência de sinais proposta pelo próprio jogo, cada vez mais longa. Há quatro cores (vermelho, verde, azul e amarelo), e cada um emite um som característico.
+
+São quatro níveis de dificuldade, que indicam quantas cores deverão ser acertadas:
+
+| Dificuldade | Tamanho da sequência |
+| :---------: | :------------------: |
+|      1      |       8 rodadas      |
+|      2      |      14 rodadas      |
+|      3      |      20 rodadas      |
+|      4      |      31 rodadas      |
+
+Depois que o usuário seleciona a dificuldade, o jogo irá iniciar uma rodada com uma cor e cada rodada adiciona uma cor 
+
+```
+Rodada 1 de 8
+Memorize a sequência:
+-> Amarelo
+```
+O usuario terá que memorizar a sequência e digitar a letra correspndente com a cor  
+
+```
+Agora é sua vez! Digite a sequência usando:
+R = Vermelho | G = Verde | Y = Amarelo | B = Azul 
+Botão #1 = Y
+```
+
+Caso o usuário acerte a cor, ele passará para a próxima rodada, onde a cor anterior será mostrada juntamente de uma nova cor, e assim sucessivamente.
+
+```
+Rodada 2 de 8
+Memorize a sequência:
+-> Amarelo
+-> Azul
+```
+
+Caso o usuário erre, será mostrado a sequência correta que deveria ser digitada e o jogo finaliza.
+
+```
+Agora é sua vez! Digite a sequência usando:
+R = Vermelho | G = Verde | Y = Amarelo | B = Azul 
+Botão #1 = G
+Errou! O correto era Amarelo. Você perdeu!
+```
+
+Obsevação: Qualquer valor inserido fora das letras correspondente as cores fechará o progama e serã considerado derrota
+
+```
+Agora é sua vez! Digite a sequência usando:
+R = Vermelho | G = Verde | Y = Amarelo | B = Azul 
+Botão #1 = K
+Entrada invalida. Você perdeu!
+```
+
+O jogo se estenderá até que o usuário vença todas as rodadas delimitadas pela dificuldade selecionada ou erre a sequência.
+
+## _Download_
+
+- 
+
+## Execução
+
+Caso não seja utilizado o windows (Não recomendado, pois o programa utiliza recursos disponíveis apenas no windows), execute o programa utilizando o seguinte comando:
+
+```
+dotnet Genius.dll
+```
+
+## Criado por 
+
+- [Danilo Costa dos Reis]  (https://github.com/DandanCosta)
+- [Carlos Eduardo Coutinho Paredes] (https://github.com/CarlosEduardo113)
+
+---
+
+Fontes: [Manual](https://statics-submarino.b2w.io/manuais/111703711.pdf); [_Reverse Engineering an MB Electronic Simon Game_, da Waiting for Friday](<https://www.waitingforfriday.com/?p=586#:~:text=On%20the%20full%E2%80%90size%20version%20of%20Simon%20the%20lights%20are,B3%20(true%20pitch%20247.942%20Hz)>).
